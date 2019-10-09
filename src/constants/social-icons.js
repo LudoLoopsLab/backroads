e3eb69481a73bafaadf6ced9ace3bb01dac93804
Ludo <loops@loops.ovh>
2019-10-10 01:57:15 +0200

    feat(navbar, global): Add navbar hamburger logo
    Add navbar logo on smaller screen
    Add links.js, to make links as variable
    Add social-icons.js to keep social links all the same

---
+++ src/constants/social-icons.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { FaFacebook, FaTwitterSquare, FaSquarespace } from "react-icons/fa"
+
+export default [
+  {
+    icons: <FaFacebook />,
+    url: "https://twitter.com",
+  },
+  {
+    icons: <FaTwitterSquare />,
+    url: "https://twitter.com",
+  },
+  {
+    icons: <FaSquarespace />,
+    url: "https://twitter.com",
+  },
+]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { FaAdobe } from "react-icons/fa"
 export default () => (
   <Layout>
     <div>Hello from backroads</div>
+    <FaAdobe />
   </Layout>
 )
