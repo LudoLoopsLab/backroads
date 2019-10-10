459727066b49bcb1fb22548906fc81e1585ad5d2
Ludo <loops@loops.ovh>
2019-10-10 02:33:32 +0200

    fix(navbar): fix menu error
    Didn't showed item menu on small screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,20 +7,34 @@ import socialIcons from "../constants/social-icons"
 import logo from "../images/logo.svg"
 
 const Navbar = () => {
-  const { isOpen, setNav } = useState(false)
+  const [isOpen, setNav] = useState(false)
   const toggleNav = () => {
     setNav(isOpen => !isOpen)
   }
-
   return (
     <nav className={styles.navbar}>
       <div className={styles.navCenter}>
         <div className={styles.navHeader}>
           <img src={logo} alt="backroads logo" />
           <button type="button" className={styles.logoBtn} onClick={toggleNav}>
-            <FaAlignRight className={styles.logoIcon} />>
+            <FaAlignRight className={styles.logoIcon} />
           </button>
         </div>
+        <ul
+          className={
+            isOpen
+              ? `${styles.navLinks} ${styles.showNav}`
+              : `${styles.navLinks}`
+          }
+        >
+          {links.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link to={item.path}>{item.text}</Link>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </nav>
   )
