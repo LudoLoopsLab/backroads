ac098aaa680f21abcd76888e6fa61ef8637dd1b1
Ludo <loops@loops.ovh>
2019-10-15 18:23:47 +0200

    Feat(404.js): add 404 page with custom message and with link to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
 export default () => (
   <Layout>
-    <SimpleHero />
+    <SimpleHero>
+      <Banner
+        title="continue exploring"
+        info=" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, odit!"
+      >
+        <Link to="/tours" className="btn-white">
+          Explore tours
+        </Link>
+      </Banner>
+    </SimpleHero>
   </Layout>
 )
