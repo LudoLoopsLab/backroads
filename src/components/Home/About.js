30e5338fd4cbb37f6ae86d19b0912652b65ee069
Ludo <loops@loops.ovh>
2019-10-15 22:17:02 +0200

    feat(about.js):  add image to about.js
    have to import the image, then be able to use {img} shortcut.
    add some style to button read more

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,13 +1,30 @@
 import React from "react"
 import Title from "../Title"
-import styled from "styled-components"
+import styles from "../../css/about.module.css"
+import img from "../../images/defaultBcg.jpeg"
+
 const About = () => {
   return (
-    <div>
-      <h4>
-        <Title title="about" subtitle="us" />
-      </h4>
-    </div>
+    <section className={styles.about}>
+      <Title title="about" subtitle="us" />
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={img} alt="about company" />
+          </div>
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>expore the difference</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+            aliquid recusandae reiciendis.
+          </p>
+          <button type="button" className="btn-primary">
+            read more
+          </button>
+        </article>
+      </div>
+    </section>
   )
 }
 
