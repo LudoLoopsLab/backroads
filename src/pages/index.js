c539de1d34b23843df8b509870e65babdc64a9b4
Ludo <loops@loops.ovh>
2019-10-15 17:54:06 +0200

    Feat(index.js): add simple hero picture

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import React from "react"
 // import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import { FaAdobe } from "react-icons/fa"
+import SimpleHero from "../components/SimpleHero"
 export default () => (
   <Layout>
-    <div>Hello from backroads</div>
-    <FaAdobe />
+    <SimpleHero />
   </Layout>
 )
